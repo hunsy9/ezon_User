49b373c02740e264e933ba2ad1ba340137b06cc7
유승훈 <101303791+juniper0917@users.noreply.github.com>
2023-09-17 17:10:34 +0900

    init

---
+++ src/test/java/com/blockcoding/ezon/EzonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blockcoding.ezon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EzonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
